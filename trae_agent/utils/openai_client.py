ce13519e4e3061b9845ed8f163b49fda41c145fd
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-07-07 11:17:31 +0700

    Update model name to `o4-mini` for `tool_capable_models` list

--- trae_agent/utils/openai_client.py
+++ trae_agent/utils/openai_client.py
@@ -154,7 +154,7 @@ class OpenAIClient(BaseLLMClient):
         tool_capable_models = [
             "gpt-4-turbo", "gpt-4o", "gpt-4o-mini",
             "gpt-4.1", "gpt-4.5",
-            "o1", "o3", "o4"
+            "o1", "o3", "o4-mini"
         ]
         return any(model in model_parameters.model for model in tool_capable_models)
 
